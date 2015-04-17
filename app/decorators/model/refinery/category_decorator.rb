ac94c26affe2fae40b569fb054e1e980cd4f359b
Ross Fuhrman <rossfuhrman@gmail.com>
2015-04-17 13:18:20 -0500

    Add spec related methods to products and categories

--- app/decorators/model/refinery/category_decorator.rb
+++ app/decorators/model/refinery/category_decorator.rb
@@ -1,15 +1,36 @@
-#not sure if the path on this is right
-#seems to be working
 Refinery::Products::Category.class_eval do
-
-  #self.table_name = 'refinery_products_categories'
-  #self.primary_key = 'id_from_vendor'
 
   has_many :spec_categories, :class_name => '::Refinery::ProductSpecs::SpecCategory', :foreign_key => 'category_id'
   has_many :images, :as => :imagable, :class_name => '::Refinery::Products::ProductImage', :dependent => :destroy
 
   def custom_slug_or_title
     self.title
+  end
+
+  def specs
+    (parent.blank? ? ActiveSupport::OrderedHash.new : parent.specs).tap do |hash|
+      spec_groups.each { |g| hash[g] = g.specs }
+    end
+  end
+
+  def flattened_specs
+    spec_groups.map(&:specs).flatten.sort_by(&:priority_sequence)
+  end
+
+  def default_spec_columns
+    default_specs.map &:name
+  end
+
+  def default_specs
+    specs.values.flatten[0..2]
+  end
+
+  def sort_options(count)
+    begin
+      products.first.specs.values.first.keys.first(count).map{|s| [s.name.upcase, s.id_from_vendor]}
+    rescue
+      []
+    end
   end
 
 end
--- app/decorators/model/refinery/product_decorator.rb
+++ app/decorators/model/refinery/product_decorator.rb
@@ -1,14 +1,47 @@
-#not sure if the path on this is right
-#seems to be working
 Refinery::Products::Product.class_eval do
-  #this is wrong :(
-  #has_many :specs, :class_name => '::Refinery::ProductSpecs::ProductSpec', :foreign_key => 'refinery_product_id'
   
   has_many :spec_values, :class_name => '::Refinery::ProductSpecs::SpecValue', :foreign_key => 'product_id', :dependent => :destroy
   has_many :images, :as => :imagable, :class_name => '::Refinery::Products::ProductImage', :dependent => :destroy
 
+  attr_accessor :flattened_specs
+
   def display_name
     self.long_name || self.name || self.non_display_name || nil
+  end
+
+  def specs
+    parent_category.specs.tap do |group_hash|
+      group_hash.each_pair do |key, value_array|
+        value_hash = ActiveSupport::OrderedHash.new
+        value_array.each do |spec|
+          value_hash[spec] = spec_value_for(spec)
+        end
+        group_hash[key] = value_hash
+      end
+    end
+  end
+
+  def flattened_specs(count = 10)
+    specs = parent_category.flattened_specs
+    specs = specs.first(count) if count
+    value_hash = ActiveSupport::OrderedHash.new
+
+    specs.each do |spec|
+      value = spec_value_for(spec)
+      value_hash[spec] = value #unless value.blank?
+    end
+
+    value_hash
+  end
+
+  def spec_value_for(spec)
+    spec_values.select {|v|
+      (v.id_from_vendor && v.id_from_vendor == spec.id_from_vendor)
+    }.first
+  end
+
+  def formatted_spec_value_for(spec, short = false)
+    spec_value_for(spec).try(:formatted_value, spec, short)
   end
 
 end
